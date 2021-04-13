455117096ad9bb2540b195028426edb1f421e17f
tamanegi07 <tamanegi322@gmail.com>
2021-04-13 23:35:55 +0900

    add-ui

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public UnityEngine.UI.Text scoreLabel;
+	public void Update ()
+	{
+		int count = GameObject.FindGameObjectsWithTag ("item").Length;
+		scoreLabel.text = count.ToString ();
+	}
+}
